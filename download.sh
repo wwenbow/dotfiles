6236dced12ba45500c5ed407f303b6d05c8b9641
Wenbo Wang <wwenbow@gmail.com>
2014-04-23 17:18:24 -0700

    modified install locations and added guake solarized

--- download.sh
+++ download.sh
@@ -5,15 +5,20 @@
 #############################
 
 # Download oh-my-zsh
-if [ ! -d .oh-my-zsh/ ]; then
-    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/dotfiles/.oh-my-zsh
+if [ ! -d ~/.oh-my-zsh/ ]; then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 # copy old agnoster theme to oh-my-zsh themes
-if [ ! -e ~/dotfiles/.oh-my-zsh/themes/agnoster-old.zsh-themes ]; then
-    cp agnoster-theme/agnoster-old.zsh-theme ~/dotfiles/.oh-my-zsh/themes/
+if [ ! -e ~/.oh-my-zsh/themes/agnoster-old.zsh-themes ]; then
+    cp agnoster-theme/agnoster-old.zsh-theme ~/.oh-my-zsh/themes/
 fi
 # Download zsh-syntax-highlight
-if [ ! -d .oh-my-zsh/plugins/zsh-syntax-highlighting/ ]; then
+if [ ! -d ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/ ]; then
     git clone git://github.com/zsh-users/zsh-syntax-highlighting.git \
-        ~/dotfiles/.oh-my-zsh/plugins/zsh-syntax-highlighting
+        ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+fi
+
+# Download and install fzf
+if [ ! -d ~/.fzf/ ]; then
+    git clone https://github.com/junegunn/fzf.git ~/.fzf
 fi
