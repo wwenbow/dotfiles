c358243eba5945be321bbbf092b27fde6329564f
Wenbo Wang <wwenbow@gmail.com>
2018-05-30 16:18:52 -0400

    better bash

--- download.sh
+++ download.sh
@@ -12,6 +12,11 @@ if [ ! -d ~/.oh-my-zsh/ ]; then
    git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
 fi
 
+# Download bash_it
+if [ ! -d ~/bash_it/ ]; then
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+fi
+
 # Downlaod tmux plugin manager
 if [ ! -d ~/.tmux/plugins/tpm/ ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
